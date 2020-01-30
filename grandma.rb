21fff6caa4cfa181e25273cb30f23e848ef3e3f6
Camisha  Farris <cfarris@anointedflooring.com>
2020-01-30 19:29:14 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
  "I LOVE YOU TOO PUMPKIN"
- elsif phrase == 'WHAT DID YOU EAT TODAY '
+ elsif phrase == "WHAT DID YOU EAT TODAY '
 else 
   "HUH? SPEAK UP, SONNY!"
   end
