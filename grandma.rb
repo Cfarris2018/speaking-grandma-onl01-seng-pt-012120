91602333ebcef1c9e5888384c9ca6f0a25ae33f7
Camisha  Farris <cfarris@anointedflooring.com>
2020-01-30 19:26:49 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,12 @@
 
 def speak_to_grandma(phrase)
- "speak_to_grandma".downcase
-if "speak_to_grandma".downcase ==
-  puts "HUH?! SPEAK UP, SONNY!" 
-elsif == speak_to_grandma = "I LOVE YOU GRANDMA!"
-  puts "I LOVE YOU  PUMKIN!"
-else
-  puts "NO NOT SINCE 1938!"
+  if phrase == "I LOVE YOU GRANDMA!"
+ "I LOVE YOU TOO PUMPKIN"
+else 
+  "HUH? SPEAK UP, SONNY!"
+  end
 end
+
+
+  
 
